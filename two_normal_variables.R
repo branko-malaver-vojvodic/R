86ba729a0e3b6a70e06afee234f96a0e2dcccd25
Branko <bmala060@uottawa.ca>
2020-01-17 09:46:49 -0500

    Py

---
+++ two_normal_variables.R
@@ -0,0 +1,25 @@
+lcb <- (-20)
+ucb <- (40)
+
+u <- seq(from = lcb,
+         to = ucb,
+         length.out = 1e+5)
+v1 <- dnorm(x = u,
+            mean = 4.6,
+            sd = 4)
+v2 <- dnorm(x = u,
+            mean = 11.5,
+            sd = sqrt(70))
+
+matplot(x = u,
+        y = cbind(v1, v2),
+        type = "l",
+        lty = 1,
+        col = c("red", "blue"),
+        xlab = "Valeur",
+        ylab = "Densit�",
+        main = "Exercise 1.1")
+legend(x = "topright",
+       legend = paste("Distbn X", 1:2),
+       col = c("red", "blue"),
+       lty = 1)
